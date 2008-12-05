847434170191ba24bda48f99427f319e51ee737b
cho45 <cho45@lowreal.net>
2008-12-06 04:44:31 +0900

    グラフを追加

--- damage-script.js
+++ damage-script.js
@@ -1,6 +1,9 @@
 
 $E = createElementFromString;
 
+function log (m) {
+	if (console.log) console.log(m);
+}
 
 
 function MabinogiDamageCalculator () {
@@ -20,22 +23,32 @@ MabinogiDamageCalculator.prototype = {
 				}
 			});
 		});
+
+		self.calc();
 	},
 
 	calc : function () {
+		var self = this;
+
+		var graph  = [];
 		var expdmg = expectation(
-			Number(this.form.min.value),
-			Number(this.form.max.value),
-			Number(this.form.balance.value)
+			Number(self.form.min.value),
+			Number(self.form.max.value),
+			Number(self.form.balance.value),
+			function (pr, i) {
+				graph.push({ dmg: i, pr: pr });
+			}
 		);
 
-		$(this.form.expectation).html(expdmg.toFixed(2));
+		self.drawGraph(graph);
 
-		var critical  = Number(this.form.critical.value) / 100;
-		var adddamage = Number(this.form.max.value) * (MabinogiDamageCalculator.CriticalRank[this.form.criticalrank.value] / 100);
+		$(self.form.expectation).html(expdmg.toFixed(2));
+
+		var critical  = Number(self.form.critical.value) / 100;
+		var adddamage = Number(self.form.max.value) * (MabinogiDamageCalculator.CriticalRank[self.form.criticalrank.value] / 100);
 
 		var expdmgwithcri = expdmg + adddamage * critical;
-		$(this.form.criticalexpectation).html(expdmgwithcri.toFixed(2));
+		$(self.form.criticalexpectation).html(expdmgwithcri.toFixed(2));
 
 
 		// 正規分布の累積分布関数生成関数
@@ -50,7 +63,7 @@ MabinogiDamageCalculator.prototype = {
 			};
 		}
 
-		function expectation (min, max, balance) {
+		function expectation (min, max, balance, cb) {
 			if (min > max) max = min;
 
 			var variance = 0.0835 * Math.pow(max - min, 2);
@@ -67,9 +80,11 @@ MabinogiDamageCalculator.prototype = {
 				}
 			};
 
-			var ret = 0;
+			var ret = 0, t = 0;
 			for (var i = min; i <= max; i++) {
-				ret += pr(i) * i;
+				t = pr(i);
+				cb(t, i);
+				ret += t * i;
 			}
 
 			return ret;
@@ -77,6 +92,18 @@ MabinogiDamageCalculator.prototype = {
 
 
 	},
+
+	drawGraph : function (graph) {
+		var self = this;
+		$(self.form.graph).empty();
+
+		var max = graph.slice(0).sort(function (a, b) { return b.pr - a.pr })[0];
+		for (var i = 0; i < graph.length; i++) {
+			var d = graph[i];
+			var bar = $E("<div class='graphbar' title='#{dmg} #{pr}'>#{dmg}</div>", { parent: self.form.graph, data: d });
+			$(bar.root).css("width", (d.pr / max.pr) * 100 + "%");
+		}
+	}
 };
 MabinogiDamageCalculator.Inputs = ["min", "max", "critical", "criticalrank", "balance"];
 MabinogiDamageCalculator.CriticalRank = {
