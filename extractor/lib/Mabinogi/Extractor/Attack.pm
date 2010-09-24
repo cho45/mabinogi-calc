ceefc74f565386e230e5ae98f2c8bee099758b96
cho45 <cho45@lowreal.net>
2010-09-25 05:13:01 +0900

    最適化

--- extractor/lib/Mabinogi/Extractor/Attack.pm
+++ extractor/lib/Mabinogi/Extractor/Attack.pm
@@ -11,9 +11,11 @@ use Carp;
 use Imager;
 use Inline with => 'Imager';
 use Inline C => <<'EOS';
-void template_match(Imager::ImgRaw img, Imager::ImgRaw tmpl) {
+void template_match(Imager::ImgRaw img, Imager::ImgRaw tmpl, int all) {
 	int x, y, tx, ty;
-	i_color img_color, tmpl_color;
+	i_color tmp_color;
+	unsigned char img_color, tmpl_color;
+	unsigned char is_white;
 	// printf("img: w:%d h:%d, tmpl: w:%d, h:%d\n", img->xsize, img->ysize, tmpl->xsize, tmpl->ysize);
 
 	Inline_Stack_Vars;
@@ -22,30 +24,27 @@ void template_match(Imager::ImgRaw img, Imager::ImgRaw tmpl) {
 	for (y = 0; y < img->ysize - tmpl->ysize; y++) for (x = 0; x < img->xsize - tmpl->xsize; x++) {
 		// if (img->ysize == 50 && y == 1) printf("(x:%d, y:%d)\n", x, y);
 		for (ty = 0; ty < tmpl->ysize; ty++) for (tx = 0; tx < tmpl->xsize; tx++) {
-			i_gpix(tmpl, tx, ty, &tmpl_color);
-			i_gpix(img, x + tx, y + ty, &img_color);
-			// if (img->ysize == 50 && y == 1 && x == 55) printf("t:%d\ti:%d (x:%d, y:%d)\n", tmpl_color.gray.gray_color, img_color.gray.gray_color, x, y);
-			if (tmpl_color.gray.gray_color == 255) {
-				if (img_color.gray.gray_color == 255) {
-					continue;
-				} else {
-					goto next;
-				}
-			} else if (tmpl_color.gray.gray_color == 0) {
-				if (img_color.gray.gray_color == 255) {
-					goto next;
-				} else {
-					continue;
-				}
-			} else {
-				continue;
+			i_gpix(tmpl, tx, ty, &tmp_color);
+			tmpl_color = tmp_color.gray.gray_color;
+			i_gpix(img, x + tx, y + ty, &tmp_color);
+			img_color  = tmp_color.gray.gray_color;
+			is_white = img_color == 255;
+			if (tmpl_color == 255 && !is_white) {
+				goto next;
+			} else
+			if (tmpl_color ==   0 &&  is_white) {
+				goto next;
 			}
 		}
 
 		Inline_Stack_Push(sv_2mortal(newSViv(x)));
 		Inline_Stack_Push(sv_2mortal(newSViv(y)));
+		if (!all) goto done;
+
 		next:;
 	}
+
+	done:;
 	Inline_Stack_Done;
 }
 EOS
@@ -55,6 +54,8 @@ sub extract {
 
 	my $tmpl = Imager->new;
 	$tmpl->read(file => $TEMPLATE_DIR->file('attack.png'));
+	$tmpl = $tmpl->crop(left => 1, top => 1); # 枝刈り用に白が最初にくるように調整
+
 	my $img = Imager->new;
 	$img->read(file => $image) or croak $img->errstr;
 	$img = $img->convert(preset => 'grey');
@@ -63,10 +64,11 @@ sub extract {
 	$DEBUG and ($img->write(file => '/tmp/test.png', type => 'png') or die $img->errstr);
 
 	my ($x, $y);
-	($x, $y) = template_match($img, $tmpl);
-	($x, $y) = template_match($img, $tmpl->filter(type=>"hardinvert")) unless defined $x;
+	($x, $y) = template_match($img, $tmpl, 0);
 	defined $x or croak "couldn't find template";
-	$img = $img->crop(left => $x + 15, top => $y + 10, width => 80, height => 55);
+	$img = $img->crop(left => $x + 15, top => $y + 10, width => 80, height => 55) or die $img->errstr;
+
+	$DEBUG and ($img->write(file => '/tmp/test.png', type => 'png') or die $img->errstr);
 
 	my $numbers = [
 		map {
@@ -81,9 +83,8 @@ sub extract {
 	];
 
 	my $chars = {};
-	$DEBUG and ($img->write(file => '/tmp/test.png', type => 'png') or die $img->errstr);
 	for my $number (@$numbers) {
-		my @matched = template_match($img, $number->{tmpl});
+		my @matched = template_match($img, $number->{tmpl}, 1);
 		my $n = $number->{n};
 		while (my ($x, $y) = splice @matched, 0, 2) {
 			$chars->{$y}{$x} = $n;
@@ -91,7 +92,7 @@ sub extract {
 	}
 
 	my $res = [];
-	for my $y (qw/3 16 29 42/) {
+	for my $y (qw/2 15 28 41/) {
 		push @$res, '';
 		for my $x (sort { $a <=> $b } keys %{ $chars->{$y} }) {
 			$res->[-1] .= $chars->{$y}{$x};
